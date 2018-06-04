c332cf188aad7f9af16021bf1df8aa3325e69f30
Michael <michkna3@gmail.com>
2018-06-04 17:42:29 -0400

    Finish user edit, update actions

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get     '/login',    to: 'sessions#new'
   post    '/login',    to: 'sessions#create'
   delete  '/logout',   to: 'sessions#destroy'
-  # resources :users
+  resources :users, only: [:edit, :update, :destroy]
   resources :accounts do
     resources :comments, only: [:create, :destroy]
   end
