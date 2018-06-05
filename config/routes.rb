6e699b919ecaa8f8b24e53a47dd07bb2ba7ad3a2
Michael <michkna3@gmail.com>
2018-06-05 19:49:27 -0400

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  root 'welcome#index'
+  root    'static_pages/home'
+  get     'static_pages/home'
+  get     'static_pages/contact'
   get     '/signup',   to: 'users#new'
   post    '/signup',   to: 'users#create'
   get     '/login',    to: 'sessions#new'
   post    '/login',    to: 'sessions#create'
   delete  '/logout',   to: 'sessions#destroy'
-  resources :users, only: [:edit, :update, :destroy]
+  resources :users, only: [:new, :edit, :update, :destroy]
   resources :accounts do
     resources :comments, only: [:create, :destroy]
   end
