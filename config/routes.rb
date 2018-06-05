d6f06604de5012476051a9a1b9655581eb4f3f7c
Michael <michkna3@gmail.com>
2018-06-05 19:10:15 -0400

    add a static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   root 'welcome#index'
   get     '/signup',   to: 'users#new'
   post    '/signup',   to: 'users#create'
