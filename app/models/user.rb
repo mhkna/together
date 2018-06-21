15589d5a246aec7cccb7873e7719f19b940ae520
Michael <michkna3@gmail.com>
2018-06-20 21:55:59 -0400

    remember users

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,7 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
+  # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
   end
--- more.md
+++ more.md
@@ -2,4 +2,3 @@ Returns the hash digest of the given string
 Returns a random token
 Remembers a user in the database for use in persistent sessions
 Returns true if the given token matches the digest
-Forgets a user
