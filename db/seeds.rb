3203088cba571c528b02718ae0a9fb64206f2c47
Michael <michkna3@gmail.com>
2018-07-05 22:42:12 -0400

    seed initial round

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
+#create only initial round
+Round.create!
 
 20.times do |count|
+  count = count+1
   User.create!(username:              "example_user_#{count}",
                email:                 Faker::Internet.unique.email,
                password:              "foobar",
-               password_confirmation: "foobar")
+               password_confirmation: "foobar",
+               admin:                 true)
 
   Account.create!(website: "instagram",
                   username: Faker::Internet.unique.user_name,
@@ -12,4 +16,5 @@
 
   Comment.create!(text: Faker::MichaelScott.quote,
                   account_id: count)
+
 end
