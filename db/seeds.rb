80c2dd58d1fce76ce7712d7e6863883d2976c0be
Michael <michkna3@gmail.com>
2018-07-05 22:29:10 -0400

    populate seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
-User.create!(username:  "example_user",
-             email: "example@fgcu.edu",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true)
+
+20.times do |count|
+  User.create!(username:              "example_user_#{count}",
+               email:                 Faker::Internet.unique.email,
+               password:              "foobar",
+               password_confirmation: "foobar")
+
+  Account.create!(website: "instagram",
+                  username: Faker::Internet.unique.user_name,
+                  round_id: 1,
+                  user_id: count)
+
+  Comment.create!(text: Faker::MichaelScott.quote,
+                  account_id: count)
+end
