4982a1c9636561cb672372f28759484b82eb0690
Michael <michkna3@gmail.com>
2018-07-06 15:40:23 -0400

    create timer with interval

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -6,21 +6,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
-document.addEventListener('DOMContentLoaded', () => {
+function tick() {
+  const element = (
+    <div>
+      <h2>{new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
+    element,
+    document.getElementById('counter-container')
   )
-})
+}
+
+setInterval(tick, 1000);
