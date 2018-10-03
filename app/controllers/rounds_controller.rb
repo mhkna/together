9b64b535e105fe1dfeaac16cd23e5df0c128fa64
Michael <michkna3@gmail.com>
2018-10-02 22:21:21 -0400

    round show view only in time

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,4 +1,6 @@
 class RoundsController < ApplicationController
+  include RoundsHelper
+
   def create
     @round = Round.new
     @round.save
@@ -6,7 +8,17 @@ class RoundsController < ApplicationController
   end
 
   def show
-    @round = Round.find(params[:id])
-    @round.matches = @round.matched_accounts(current_user.id, current_user.accounts.last.match_amount)
+    in_round = in_round?(current_user)
+    time_in = Time.now.strftime('%M').to_i < 7
+    if time_in && in_round
+      @round = Round.find(params[:id])
+      @round.matches = @round.matched_accounts(current_user.id, current_user.accounts.last.match_amount)
+    elsif time_in
+      # Wait for new round
+    elsif in_round == false
+      redirect_to :controller => 'accounts', :action => 'new'
+    else
+      redirect_to :controller => 'accounts', :action => 'show', id: current_user.accounts.last.id
+    end
   end
 end
